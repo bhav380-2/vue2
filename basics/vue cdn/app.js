92228b555d827d14b28d7e84263592470a3e3142
bhav380 <bhaweshrathour18@gmail.com>
2024-08-04 15:41:24 +0530

    computed properties

--- basics/vue cdn/app.js
+++ basics/vue cdn/app.js
@@ -58,3 +58,36 @@ new Vue({
         }
     }
 })
+
+
+new Vue({
+    el:"#instance3",
+    data:{
+        age:20,
+        a:0,
+        b:0
+    },
+    methods:{
+        // addToA:function(){
+        //     return this.a+this.age
+        // },
+
+        // addToB: function(){
+        //     return this.b+this.age;
+        // }
+    },
+
+    computed:{
+
+        addToA:function(){
+            console.log("add to A")
+            return this.a+this.age
+        },
+
+        addToB: function(){
+            console.log("add to B")
+            return this.b+this.age;
+        }
+
+    }
+})
