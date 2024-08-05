d1c89d3c529d6c55e427a0117db08741d677d5e2
bhav380 <bhaweshrathour18@gmail.com>
2024-08-05 08:40:53 +0530

    props

--- projects/recordings/vue-playlist/src/App.vue
+++ projects/recordings/vue-playlist/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <app-header></app-header>
 
-    <app-ninjas></app-ninjas>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
 
 
 
@@ -29,6 +29,15 @@ export default {
 
   data(){
     return{
+
+      ninjas:[
+            {name :'Ryu',speciality: 'Vue Components', show: false},
+            {name :'Crystal',speciality: 'HTML Wizardry', show: false},
+            {name :'Hitoshi',speciality: 'Click Events', show: false},
+            {name :'Tango',speciality: 'Conditionals', show: false},
+            {name :'Kami',speciality: 'Webpack', show: false},
+            {name :'Yoshi',speciality: 'Data Diggin', show: false},
+        ]
 
 
     }
