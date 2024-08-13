b43a665dc543daed2dc921b8cb7254591dd7a1ea
bhav380 <bhaweshrathour18@gmail.com>
2024-08-13 14:01:13 +0530

    http post request

--- projects/blogApp/src/components/addBlog.vue
+++ projects/blogApp/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="add-blog">
         <h2>Add a New Blog Post</h2>
-        <form>
+        <form v-if="!submitted">
             <label>Blog Title:</label>
             <input type="text" v-model.lazy="blog.title" required />
             <label>Blog Content:</label>
@@ -24,9 +24,18 @@
                 <option v-for="author in authors"> {{author}}</option>
             </select>
 
-
-
+            <button v-on:click.prevent="post">Add Blog</button>
         </form>
+
+        <div v-if = "submitted">
+
+            <h3>Thanks for submitting this blog</h3>
+
+
+        </div>
+
+
+
         <div id="preview">
             <h3>Preview blog</h3>
             <p>Blog title: {{ blog.title }}</p>
@@ -57,10 +66,23 @@ export default {
                 categories:[],
                 author:""
             },
-            authors:['The Vue','The React','The Angular','The Redux']
+            authors:['The Vue','The React','The Angular','The Redux'],
+            submitted: false,
         }
     },
     methods: {
+
+        post:function(){
+            this.$http.post('http://jsonplaceholder.typicode.com/posts',{
+                title: this.blog.title,
+                body:this.blog.content,
+                userId: 1
+            }).then(function(data){
+                console.log(data);
+                this.submitted = true;
+
+            });
+        }
 
     }
 }
