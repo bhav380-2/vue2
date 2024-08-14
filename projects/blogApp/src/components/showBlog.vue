6076435d3105aa6329d42018e9d29bb4e213e005
bhav380 <bhaweshrathour18@gmail.com>
2024-08-14 12:43:07 +0530

    vue parameters

--- projects/blogApp/src/components/showBlog.vue
+++ projects/blogApp/src/components/showBlog.vue
@@ -4,7 +4,7 @@
         <input type = "text" v-model="search" placeholder="search blogs"/>
         <div v-for="blog in filteredBlogs" class="single-blog">
 
-            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+            <router-link v-bind:to="'/blog/' + blog.id"> <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2> </router-link>
             <article>{{ blog.body  | snippet}}</article>
 
         </div>
