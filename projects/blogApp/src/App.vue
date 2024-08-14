693ed4fe555a5dd4586b32457cba6de6af76fbea
bhav380 <bhaweshrathour18@gmail.com>
2024-08-14 12:25:14 +0530

    route links and exact attribute

--- projects/blogApp/src/App.vue
+++ projects/blogApp/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
 
+    <app-header>
+      
+    </app-header>
+
     <router-view></router-view>
       <!-- <add-blog></add-blog> -->
       <!-- <show-blogs></show-blogs>
@@ -14,12 +18,15 @@ import addBlog from './components/addBlog.vue';
 import showBlog from './components/showBlog.vue'
 import listBlogs from './components/listBlogs.vue'
 
+import header from './components/header.vue'
+
 
 export default {
   components: {
       'add-blog': addBlog,
       'show-blogs': showBlog,
-      'list-blogs':listBlogs
+      'list-blogs':listBlogs,
+      'app-header': header
   },
   data () {
       return {
